6202a8c31f6e33ef499a254a79a25d6325af3516
mushikakazuki-1202 <kmushika1202@yahoo.co.jp>
2021-06-12 14:54:17 +0900

    ♻️ Main部分のコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Footer } from '../components/Footer'
-import { Headline } from '../components/Headline'
-import { Links } from '../components/Links'
 import styles from '../styles/Home.module.css'
+
+import { Footer } from "../components/Footer"
+import { Main } from '../components/Main'
 
 export default function Home() {
   return (
@@ -14,11 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Headline title="about" />
-
-        <Links />
-      </main>
+      <Main page="about" />
       <Footer />
     </div>
   )
