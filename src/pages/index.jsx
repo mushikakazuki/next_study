57259ffdb9054263a7439a256b348a323b3c72a4
mushikakazuki-1202 <kmushika1202@yahoo.co.jp>
2021-06-13 16:12:29 +0900

    ⚗️クリックイベント練習用

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,22 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1000;
+  const handleClick = useCallback((e) => {
+    console.log("test");
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
