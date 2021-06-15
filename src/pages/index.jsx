9d6fd8c83fd6322932067f5a34075c34e302affa
mushikakazuki-1202 <kmushika1202@yahoo.co.jp>
2021-06-15 22:20:19 +0900

    ⚡️ useEffect,usecallbacの第二引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,13 +5,15 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [foo, setfoo] = useState(1);
-  const handleClick = (e) => {
-    setfoo((foo) => foo + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (foo < 10) {
+      setfoo((foo) => foo + 1);
+    }
+  }, [foo]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
